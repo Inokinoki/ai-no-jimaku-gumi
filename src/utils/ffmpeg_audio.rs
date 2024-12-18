94cd022bc80971fd806449fe67e226f8cbe8e537
Inoki <veyx.shaw@gmail.com>
2024-12-18 22:20:19 +0100

    Add first workable f32 conversion with single channel

--- src/utils/ffmpeg_audio.rs
+++ src/utils/ffmpeg_audio.rs
@@ -100,8 +100,19 @@ fn extract_audio_from_video(video_path: &str, audio_path: &str) {
             let mut decoded = frame::Audio::empty();
             decoder.send_packet(&packet).unwrap();
             while decoder.receive_frame(&mut decoded).is_ok() {
+                let format = decoded.format();
+                unsafe {
+                    println!(
+                        "Decoded frame line size: {:?}",
+                        (*decoded.as_ptr()).linesize[0]
+                    );
+                }
+
                 println!("Decoded frame format: {:?}", decoded.format());
                 println!("Decoded frame channels: {:?}", decoded.channels());
+                println!("Decoded frame duration: {}", decoded.packet().duration);
+                println!("Decoded frame position: {}", decoded.packet().position);
+                println!("Decoded frame size: {}", decoded.packet().size);
                 println!(
                     "Decoded frame channel layout: {:?}",
                     decoded.channel_layout()
@@ -110,8 +121,15 @@ fn extract_audio_from_video(video_path: &str, audio_path: &str) {
                 println!("Decoded frame pts: {:?}", decoded.pts());
                 println!("Decoded frame planes: {:?}", decoded.planes());
                 println!("Decoded frame is packed: {}", decoded.is_packed());
+                for i in 0..decoded.channels() as usize {
+                    println!("\tDecoded frame data size: {:?}", decoded.data(i).len());
+                }
                 let decoded_samples = decoded.data(0);
-                println!("Decoded {} samples", decoded_samples.len());
+                println!(
+                    "Decoded {} samples {}",
+                    decoded_samples.len(),
+                    decoded.samples()
+                );
                 let decoded_samples = decoded.data(0);
                 match decoded.format() {
                     ffmpeg::format::Sample::U8(_) => {
@@ -132,8 +150,15 @@ fn extract_audio_from_video(video_path: &str, audio_path: &str) {
                         // samples.push(converted_samples);
                     }
                     ffmpeg::format::Sample::F32(planar) => {
-                        let mut converted_samples = Vec::with_capacity(decoded_samples.len() / 4);
+                        let mut converted_samples = Vec::with_capacity(decoded.samples());
+                        let mut count = 0;
                         for chunk in decoded_samples.chunks(4) {
+                            if count < decoded.samples() {
+                                // We just pick the first channel for now
+                                count += 1;
+                            } else {
+                                break;
+                            }
                             let sample =
                                 f32::from_le_bytes([chunk[0], chunk[1], chunk[2], chunk[3]]);
                             converted_samples.push(sample);
@@ -141,8 +166,43 @@ fn extract_audio_from_video(video_path: &str, audio_path: &str) {
                         samples.extend(converted_samples.iter());
                     }
                     ffmpeg::format::Sample::F64(planar) => {
-                        // let converted_samples = decoded_samples.iter().map(|&x| x as f32).collect::<Vec<_>>();
-                        // samples.push(converted_samples);
+                        let mut converted_samples = Vec::with_capacity(decoded.samples());
+                        let mut count = 0;
+                        let mut remaining_channels = decoded.channels();
+                        for chunk in decoded_samples.chunks(8) {
+                            match planar {
+                                format::sample::Type::Packed => {
+                                    if count < decoded.samples() {
+                                        if remaining_channels > 0 {
+                                            remaining_channels -= 1;
+                                            continue;
+                                        }
+                                        count += 1;
+                                    } else {
+                                        break;
+                                    }
+                                    let sample = f64::from_le_bytes([
+                                        chunk[0], chunk[1], chunk[2], chunk[3], chunk[4], chunk[5],
+                                        chunk[6], chunk[7],
+                                    ]);
+                                    converted_samples.push(sample as f32);
+                                    // Reset remaining channels
+                                    remaining_channels = decoded.channels() - 1;
+                                }
+                                format::sample::Type::Planar => {
+                                    if count < decoded.samples() {
+                                        count += 1;
+                                    } else {
+                                        break;
+                                    }
+                                    let sample = f64::from_le_bytes([
+                                        chunk[0], chunk[1], chunk[2], chunk[3], chunk[4], chunk[5],
+                                        chunk[6], chunk[7],
+                                    ]);
+                                    converted_samples.push(sample as f32);
+                                }
+                            }
+                        }
                     }
                     ffmpeg_next::format::Sample::None => {
                         panic!("No sample format found");
@@ -155,11 +215,7 @@ fn extract_audio_from_video(video_path: &str, audio_path: &str) {
     let mut writer = hound::WavWriter::create(
         audio_path,
         hound::WavSpec {
-            channels: if channel_layout.nb_channels < 0 {
-                0
-            } else {
-                channel_layout.nb_channels
-            } as u16,
+            channels: if channel_layout.nb_channels < 0 { 0 } else { 1 } as u16,
             sample_rate: sample_rate,
             bits_per_sample: 32,
             sample_format: hound::SampleFormat::Float,
