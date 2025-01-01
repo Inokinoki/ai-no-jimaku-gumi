c78fb76866f02de87c85e82a1e4baae10964a9eb
Inoki <veyx.shaw@gmail.com>
2025-01-01 17:23:07 +0100

    Fix SRT format

--- src/output/srt.rs
+++ src/output/srt.rs
@@ -13,13 +13,27 @@ impl SrtSubTitleExporter {
     }
 }
 
+fn format_time(time: f32) -> String {
+    format!(
+        "{:02}:{:02}:{:02},{:03}",
+        (time as u32 / 3600),
+        (time as u32 % 3600 / 60),
+        ((time % 60.0).floor() as u32),
+        ((time.fract() * 1000.0) as u32)
+    )
+}
+
 impl OutputSubtitles for SrtSubTitleExporter {
     fn output_subtitles(&mut self, subtitles: Vec<Subtitle>) {
         let mut srt = String::new();
         for (i, subtitle) in subtitles.iter().enumerate() {
             srt.push_str(&(i + 1).to_string());
             srt.push_str("\n");
-            srt.push_str(&format!("{:.3} --> {:.3}", subtitle.start, subtitle.end));
+            srt.push_str(&format!(
+                "{} --> {}",
+                format_time(subtitle.start),
+                format_time(subtitle.end)
+            ));
             srt.push_str("\n");
             srt.push_str(subtitle.text.trim());
             srt.push_str("\n\n");
@@ -57,6 +71,12 @@ fn test_output_subtitles() {
     file.read_to_string(&mut contents).unwrap();
     assert_eq!(
         contents,
-        "1\n0.000 --> 1.000\nHello, world!\n\n2\n1.000 --> 2.000\nGoodbye, world!\n\n"
+        format!(
+            "1\n{} --> {}\nHello, world!\n\n2\n{} --> {}\nGoodbye, world!\n\n",
+            format_time(0.0),
+            format_time(1.0),
+            format_time(1.0),
+            format_time(2.0)
+        )
     );
 }
